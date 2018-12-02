2f04f0f6faef4d9451d90d2843b343e134aaf179
Игорь Кирилюк <Barraki201@gmail.com>
2018-12-02 23:22:10 +0200

    add list add button clear list

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,85 @@
 <template>
-  <div id="app">
-    <div>
+	<div id="app">
+		<div>
 			<label>Name:</label>
 			<input type="text" v-model="name">
 			<button @click="submitName()">Add</button>
+			<button @click="clearList(names)">Clear all</button>
 		</div>
-  </div>
+		<div>
+			<ul>
+				<li v-for="personName of names" v-bind:key="personName['.key']"> {{ personName.name }} </li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
-import { namesRef } from './firebase'
-console.log( namesRef )
-
-export default {
-  data () {
-    return {
-      name: 'Paul'
-    }
-	},
-	methods: {
-		submitName() {
-			namesRef.push({ name: this.name, edit: false});
+	import {
+		namesRef
+	} from './firebase'
+	console.log(namesRef)
+	
+	export default {
+		data() {
+			return {
+				name: 'Paul'
+			}
 		},
+		firebase: {
+			names: namesRef
+		},
+		methods: {
+			submitName() {
+				namesRef.push({
+					name: this.name,
+					edit: false
+				});
+			},
+			clearList() {
+				namesRef.remove();
+				namesRef.remove()
+					console.log(namesRef)
+			},
+	
+			////   Написать функцию очистки списка с подтверждением    //////
+	
+		}
 	}
-}
+
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-button {
-	background-color: transparent;
-	border: 2px solid black;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+	
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	
+	li {
+		margin: 10px 10px;
+	}
+	
+	a {
+		color: #42b983;
+	}
+	
+	button {
+		background-color: transparent;
+		border: 2px solid black;
+	}
 </style>
