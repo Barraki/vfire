2dec1d1c69bcd56fbbfd20b366afc1bb1cd2ad6e
Игорь Кирилюк <Barraki201@gmail.com>
2018-12-02 23:59:36 +0200

    add style for input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
 	<div id="app">
+		<h1 class="h1">Add your tasks</h1>
 		<div>
 			<label>Name:</label>
 			<input type="text" v-model="name">
-			<button @click="submitName()">Add</button>
+			<button @click="submitName()" key="enter">Add</button>
 			<button @click="clearList(names)">Clear all</button>
 		</div>
 		<div>
@@ -39,15 +40,13 @@
 			clearList() {
 				namesRef.remove();
 				namesRef.remove()
-					console.log(namesRef)
+				console.log(namesRef)
 			},
 	
 			////   Написать функцию очистки списка с подтверждением    //////
 	
 		}
 	}
-
-
 </script>
 
 <style>
@@ -58,6 +57,30 @@
 		text-align: center;
 		color: #2c3e50;
 		margin-top: 60px;
+	}
+	
+	h1.h1 {
+		margin-bottom: 50px;
+	}
+	
+	input {
+		background: #fff;
+		display: flex;
+		border-radius: 8px;
+		border: 1px solid #dfe1e5;
+		box-shadow: none;
+		height: 39px;
+		width: 632px;
+		border-radius: 24px;
+		width: 630px;
+		z-index: 3;
+		height: 44px;
+		margin: 0 auto;
+		align-items: center;
+		flex: 1;
+		display: flex;
+		padding: 0 4px 0 16px;
+		padding-left: 20px;
 	}
 	
 	h1,
